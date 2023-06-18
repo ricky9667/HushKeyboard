d5ab8c2bd610330f492ab604a183b6696a4a1de5
Ricky Hu <ricky9667@gmail.com>
2023-06-18 21:14:32 +0800

    refactor: 💡 remove unused composables

--- app/src/main/java/com/rickyhu/hushkeyboard/MainActivity.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/MainActivity.kt
@@ -10,15 +10,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rickyhu.hushkeyboard.ui.theme.HushKeyboardTheme
 
@@ -47,21 +44,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HushKeyboardTheme {
-        Greeting("Android")
     }
 }
