b5bf11bc5a04a2c31905c6b0d70240115b199c5c
Ricky Hu <ricky9667@gmail.com>
2023-06-18 21:17:03 +0800

    feat: 🎸 add basic keys to HushKeyboard

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/composables/HushKeyboard.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/composables/HushKeyboard.kt
@@ -1,6 +1,8 @@
 package com.rickyhu.hushkeyboard.ui.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,6 +12,23 @@ fun HushKeyboard() {
     Column(
         modifier = Modifier.fillMaxWidth()
     ) {
-        KeyButton("R")
+        val keyboardKeys = listOf(
+            listOf("R", "U", "F", "L", "D", "B"),
+            listOf("M", "E", "S", "x", "y", "z")
+        )
+
+        for (keysRow in keyboardKeys) {
+            KeyButtonsRow(keysRow)
+        }
+    }
+}
+
+@Composable
+fun KeyButtonsRow(keys: List<String>) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        keys.forEach { KeyButton(it) }
     }
 }
