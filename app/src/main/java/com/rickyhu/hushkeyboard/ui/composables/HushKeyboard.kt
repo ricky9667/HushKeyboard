6069ca327e5756a1eaefb5c3eadd85bcba270a91
Ricky Hu <ricky9667@gmail.com>
2023-06-22 13:51:38 +0800

    refactor: 💡 extract original KeyButton as NotationKeyButton
    KeyButton is a more basic implementation

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/composables/HushKeyboard.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/composables/HushKeyboard.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.rickyhu.hushkeyboard.model.CubeKey
 import com.rickyhu.hushkeyboard.model.Notation
 
@@ -45,6 +48,13 @@ fun KeyButtonsRow(keys: List<CubeKey>) {
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.Center
     ) {
-        keys.forEach { key -> KeyButton(key) }
+        keys.forEach { key ->
+            NotationKeyButton(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(48.dp),
+                key = key
+            )
+        }
     }
 }
