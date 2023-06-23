dcb97af9b041b11d39664ab333e75101b73eb552
Ricky Hu <ricky9667@gmail.com>
2023-06-24 00:57:12 +0800

    feat: 🎸 make keys vibrate on tap

--- app/src/main/java/com/rickyhu/hushkeyboard/viewmodel/KeyboardViewModel.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/viewmodel/KeyboardViewModel.kt
@@ -1,9 +1,15 @@
 package com.rickyhu.hushkeyboard.viewmodel
 
+import android.content.Context
+import android.os.Build
+import android.os.CombinedVibration
+import android.os.VibrationEffect
+import android.os.VibratorManager
 import android.text.TextUtils
-import android.view.inputmethod.InputConnection
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.rickyhu.hushkeyboard.model.Turns
+import com.rickyhu.hushkeyboard.service.HushIMEService
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -12,13 +18,13 @@ class KeyboardViewModel : ViewModel() {
     private val _keyboardState = MutableStateFlow(KeyboardState())
     val keyboardState = _keyboardState.asStateFlow()
 
-    fun switchCounterClockwise() {
+    fun switchCounterClockwise(context: Context) {
         _keyboardState.update { state ->
             state.copy(isCounterClockwise = !state.isCounterClockwise)
         }
     }
 
-    fun switchTurns() {
+    fun switchTurns(context: Context) {
         _keyboardState.update { state ->
             when (state.turns) {
                 Turns.Single -> state.copy(turns = Turns.Double)
@@ -28,23 +34,50 @@ class KeyboardViewModel : ViewModel() {
         }
     }
 
-    fun switchWideTurn() {
+    fun switchWideTurn(context: Context) {
         _keyboardState.update { state ->
             state.copy(isWideTurn = !state.isWideTurn)
         }
     }
 
-    fun deleteText(inputConnection: InputConnection) {
+    fun deleteText(context: Context) {
+        val inputConnection = (context as HushIMEService).currentInputConnection
         val selectedText = inputConnection.getSelectedText(0)
         if (TextUtils.isEmpty(selectedText)) {
             inputConnection.deleteSurroundingText(1, 0)
         } else {
             inputConnection.commitText("", 1)
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            vibrate(context)
+        }
     }
 
-    fun inputText(inputConnection: InputConnection, text: String) {
+    fun inputText(context: Context, text: String) {
+        val inputConnection = (context as HushIMEService).currentInputConnection
         inputConnection.commitText(text, text.length)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            vibrate(context)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun vibrate(context: Context) {
+        val vibratorManager = context.getSystemService(
+            Context.VIBRATOR_MANAGER_SERVICE
+        ) as VibratorManager
+
+        val effectId = VibrationEffect.Composition.PRIMITIVE_CLICK
+
+        vibratorManager.vibrate(
+            CombinedVibration.createParallel(
+                VibrationEffect.startComposition()
+                    .addPrimitive(effectId)
+                    .compose()
+            )
+        )
     }
 }
 
