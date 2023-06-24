9b860bf0edd84a79f23f5b58e7e0041fcc9b9f6c
Ricky Hu <ricky9667@gmail.com>
2023-06-24 16:06:39 +0800

    refactor: 💡 replace pointerInput with clickable and draggable

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/composables/NotationKeyButton.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/composables/NotationKeyButton.kt
@@ -1,5 +1,10 @@
 package com.rickyhu.hushkeyboard.ui.composables
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -8,14 +13,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rickyhu.hushkeyboard.model.CubeKey
 import com.rickyhu.hushkeyboard.model.Notation
 import com.rickyhu.hushkeyboard.model.Turns
 import com.rickyhu.hushkeyboard.ui.theme.HushKeyboardTheme
-import kotlin.math.abs
 
 @Composable
 fun NotationKeyButton(
@@ -23,50 +26,45 @@ fun NotationKeyButton(
     cubeKey: CubeKey,
     onTextInput: (String) -> Unit = {}
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
     val key by rememberUpdatedState(cubeKey)
     var inputKey by remember { mutableStateOf(key) }
 
     KeyButton(
         modifier = modifier
-            .pointerInput(key) {
-                awaitPointerEventScope {
-                    while (true) {
-                        val event = awaitPointerEvent()
-                        val position = event.changes.first().position
-
-                        when (event.type.toString()) {
-                            "Press" -> {
-                                inputKey = key
-                            }
-
-                            "Move" -> {
-                                if (abs(position.x) < abs(position.y)) {
-                                    // Notate as double turn when swiping up or down, but only
-                                    // if the key state is originally a single turn.
-                                    val turns = if (key.turns == Turns.Single) {
-                                        Turns.Double
-                                    } else {
-                                        key.turns
-                                    }
-
-                                    // Clockwise down, counter-clockwise up.
-                                    inputKey = key.copy(
-                                        isCounterClockwise = position.y < 0,
-                                        turns = turns
-                                    )
-                                } else {
-                                    // Clockwise right, counter-clockwise left.
-                                    inputKey = key.copy(isCounterClockwise = position.x < 0)
-                                }
-                            }
-
-                            "Release" -> {
-                                onTextInput("$inputKey ")
-                            }
-                        }
-                    }
+            .clickable(interactionSource = interactionSource, indication = null) {
+                inputKey = key
+                onTextInput("$inputKey ")
+            }
+            .draggable(
+                interactionSource = interactionSource,
+                orientation = Orientation.Horizontal,
+                state = rememberDraggableState { x ->
+                    inputKey = key.copy(isCounterClockwise = x < 0)
+                },
+                onDragStopped = { x ->
+                    onTextInput("$inputKey ")
                 }
-            },
+            )
+            .draggable(
+                interactionSource = interactionSource,
+                orientation = Orientation.Vertical,
+                state = rememberDraggableState { y ->
+                    // Notate as double turn when swiping up or down, but only
+                    // if the key state is originally a single turn.
+                    val turns = if (key.turns == Turns.Single) {
+                        Turns.Double
+                    } else {
+                        key.turns
+                    }
+
+                    // Clockwise down, counter-clockwise up.
+                    inputKey = key.copy(isCounterClockwise = y < 0, turns = turns)
+                },
+                onDragStopped = { y ->
+                    onTextInput("$inputKey ")
+                }
+            ),
         text = key.toString()
     )
 }
