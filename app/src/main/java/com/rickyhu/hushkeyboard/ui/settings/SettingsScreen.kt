bfa214df303c2d2435df2c88c30865a2f72ab945
Ricky Hu <ricky9667@gmail.com>
2023-07-15 19:41:42 +0800

    feat: 🎸 add navigation to HomeScreen and SettingsScreen

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/settings/SettingsScreen.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/settings/SettingsScreen.kt
@@ -8,12 +8,12 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.ramcosta.composedestinations.annotation.Destination
 import com.rickyhu.hushkeyboard.ui.theme.HushKeyboardTheme
 
+@Destination
 @Composable
-fun SettingsScreen() {
-    SettingsContent()
-}
+fun SettingsScreen() = SettingsContent()
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
