2830959f7038163302dd5cb2b1d54dfca40b4254
Ricky Hu <ricky9667@gmail.com>
2023-07-15 19:40:48 +0800

    feat: 🎸 build basic SettingsScreen

---
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/settings/SettingsScreen.kt
@@ -0,0 +1,37 @@
+package com.rickyhu.hushkeyboard.ui.settings
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rickyhu.hushkeyboard.ui.theme.HushKeyboardTheme
+
+@Composable
+fun SettingsScreen() {
+    SettingsContent()
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SettingsContent() {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text("Settings") })
+        },
+        content = { padding ->
+            Text("Settings", modifier = Modifier.padding(padding))
+        }
+    )
+}
+
+@Preview
+@Composable
+fun SettingsScreenPreview() {
+    HushKeyboardTheme {
+        SettingsContent()
+    }
+}
