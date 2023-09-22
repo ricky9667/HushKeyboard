341bfad55b7ed1528b342cc06edf529cf36ab228
Ricky Hu <ricky9667@gmail.com>
2023-09-22 14:42:02 +0800

    feat: 🎸 add colors

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/theme/Color.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val LightPrimary = Color(0xFFFFFFFF)
+val LightSecondary = Color(0xFFE1E3DF)
+val LightBackground = Color(0xFFEFF3FB)
+
+val DarkPrimary = Color(0xFF38393D)
+val DarkSecondary = Color(0xFF454746)
+val DarkBackground = Color(0xFF1F1F1F)
--- app/src/main/java/com/rickyhu/hushkeyboard/ui/theme/Theme.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/theme/Theme.kt
@@ -16,15 +16,15 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DarkPrimary,
+    secondary = DarkSecondary,
+    tertiary = DarkBackground
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightPrimary,
+    secondary = LightSecondary,
+    tertiary = LightBackground
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
