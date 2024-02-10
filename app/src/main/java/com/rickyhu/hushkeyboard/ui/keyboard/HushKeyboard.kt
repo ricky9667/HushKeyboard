a22f2b98407cbb5740e792497ddbc57aee8d210a
Ricky Hu <ricky9667@gmail.com>
2024-02-10 11:06:49 +0800

    refactor: 💡 extract ControlKeyButtonRow

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/keyboard/HushKeyboard.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/keyboard/HushKeyboard.kt
@@ -6,31 +6,20 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.datastore.dataStore
-import com.rickyhu.hushkeyboard.R
 import com.rickyhu.hushkeyboard.model.NotationKeyProvider
 import com.rickyhu.hushkeyboard.settings.AppSettings
 import com.rickyhu.hushkeyboard.settings.AppSettingsSerializer
-import com.rickyhu.hushkeyboard.ui.keyboard.buttons.ControlKeyButton
 import com.rickyhu.hushkeyboard.ui.theme.DarkBackground
 import com.rickyhu.hushkeyboard.ui.theme.LightBackground
 import com.rickyhu.hushkeyboard.viewmodel.KeyboardViewModel
@@ -73,92 +62,16 @@ fun HushKeyboard(viewModel: KeyboardViewModel) {
             onTextInput = { text -> viewModel.inputText(context, text) }
         )
 
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            val controlKeyModifier = Modifier
-                .padding(4.dp)
-                .size(48.dp)
-
-            // TODO: unify button colors
-            ControlKeyButton(
-                modifier = controlKeyModifier,
-                onClick = viewModel::selectInputMethod,
-                isDarkTheme = isDarkTheme,
-                content = {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_language),
-                        tint = if (isDarkTheme) Color.White else Color.Black,
-                        contentDescription = "Language"
-                    )
-                }
-            )
-            ControlKeyButton(
-                modifier = controlKeyModifier,
-                onClick = viewModel::switchCounterClockwise,
-                isDarkTheme = isDarkTheme,
-                content = {
-                    Text(
-                        "'",
-                        color = if (isDarkTheme) Color.White else Color.Black,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            )
-            ControlKeyButton(
-                modifier = controlKeyModifier,
-                onClick = viewModel::switchTurns,
-                isDarkTheme = isDarkTheme,
-                content = {
-                    Text(
-                        keyboardState.turns.value.toString(),
-                        color = if (isDarkTheme) Color.White else Color.Black,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            )
-            ControlKeyButton(
-                modifier = controlKeyModifier,
-                onClick = viewModel::switchWideTurn,
-                isDarkTheme = isDarkTheme,
-                content = {
-                    Text(
-                        "w",
-                        color = if (isDarkTheme) Color.White else Color.Black,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            )
-            ControlKeyButton(
-                modifier = controlKeyModifier,
-                onClick = { viewModel.deleteText(context) },
-                isDarkTheme = isDarkTheme,
-                content = {
-                    Text(
-                        "⌫",
-                        color = if (isDarkTheme) Color.White else Color.Black,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center
-                    )
-                }
-            )
-            ControlKeyButton(
-                modifier = controlKeyModifier,
-                onClick = { viewModel.inputText(context, "\n") },
-                isDarkTheme = isDarkTheme,
-                content = {
-                    Icon(
-                        painter = painterResource(R.drawable.ic_return),
-                        tint = if (isDarkTheme) Color.White else Color.Black,
-                        contentDescription = "Language"
-                    )
-                }
-            )
-        }
+        ControlKeyButtonRow(
+            turns = keyboardState.turns,
+            isDarkTheme = isDarkTheme,
+            inputMethodButtonAction = viewModel::selectInputMethod,
+            rotateDirectionButtonAction = viewModel::switchCounterClockwise,
+            turnDegreeButtonAction = viewModel::switchTurns,
+            wideTurnButtonAction = viewModel::switchWideTurn,
+            deleteButtonAction = { viewModel.deleteText(context) },
+            newLineButtonAction = { viewModel.inputText(context, "\n") }
+        )
     }
 }
 
@@ -166,9 +79,5 @@ fun HushKeyboard(viewModel: KeyboardViewModel) {
 @Preview(showBackground = true)
 @Composable
 fun HushKeyboardPreview() {
-    val viewModel = KeyboardViewModel()
-
-    HushKeyboard(
-        viewModel = viewModel
-    )
+    HushKeyboard(viewModel = KeyboardViewModel())
 }
