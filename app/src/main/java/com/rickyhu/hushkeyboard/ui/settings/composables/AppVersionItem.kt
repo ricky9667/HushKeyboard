833bb1e1ab0eb7f54890fe0ab7cd9ba98f804edc
Ricky Hu <ricky9667@gmail.com>
2024-02-18 16:07:49 +0800

    Get version name from build.gradle.kts (#21)
    * feat: 🎸 get version name from build.gradle
    
    * refactor: 💡 remove redundant experimental api annotation

--- app/src/main/java/com/rickyhu/hushkeyboard/ui/settings/composables/AppVersionItem.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/ui/settings/composables/AppVersionItem.kt
@@ -3,25 +3,27 @@ package com.rickyhu.hushkeyboard.ui.settings.composables
 import androidx.compose.foundation.clickable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.rickyhu.hushkeyboard.ui.theme.HushKeyboardTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppVersionItem(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {}
 ) {
+    val context = LocalContext.current
+    val versionName = context.packageManager.getPackageInfo(context.packageName, 0).versionName
+
     ListItem(
         headlineContent = { Text("Version") },
         leadingContent = { Icon(imageVector = Icons.Default.Info, contentDescription = "Info") },
-        trailingContent = { Text("v0.2.0") },
+        trailingContent = { Text(versionName) },
         modifier = modifier.clickable(onClick = onClick)
     )
 }
