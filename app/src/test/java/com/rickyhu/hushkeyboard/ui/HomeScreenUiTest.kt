7a03a6340bebaefca9ddd4b44d1c3c8e80fbc7ca
ralap_na <lydia88na@gmail.com>
2024-05-13 20:38:48 +0800

    refactor: 💡 adjust the assertion method for HomeScreenUiTest.

--- app/src/test/java/com/rickyhu/hushkeyboard/ui/HomeScreenUiTest.kt
+++ app/src/test/java/com/rickyhu/hushkeyboard/ui/HomeScreenUiTest.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
 import com.rickyhu.hushkeyboard.home.HomeScreen
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -82,6 +83,6 @@ class HomeScreenUiTest {
             .onNodeWithText("Settings")
             .assertIsNotDisplayed()
 
-        assert(clicked)
+        assertTrue(clicked)
     }
 }
