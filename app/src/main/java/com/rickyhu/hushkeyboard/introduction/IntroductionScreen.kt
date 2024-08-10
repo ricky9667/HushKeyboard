a824b698c1e30093ee4483e4a0b0c5d365c30c79
Ricky Hu <ricky9667@gmail.com>
2024-08-10 13:54:30 +0800

    feat: 🎸 show next section when previous section finished

--- app/src/main/java/com/rickyhu/hushkeyboard/introduction/IntroductionScreen.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/introduction/IntroductionScreen.kt
@@ -4,9 +4,7 @@ import android.content.Intent
 import android.provider.Settings
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -24,6 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rickyhu.hushkeyboard.R
 import com.rickyhu.hushkeyboard.theme.HushKeyboardTheme
+import java.lang.Thread.sleep
 import splitties.systemservices.inputMethodManager
 
 @Composable
@@ -39,30 +38,38 @@ fun IntroductionScreen() {
                     .fillMaxSize()
                     .padding(paddingValues),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
                 Text(
                     text = stringResource(R.string.intro_screen_title),
-                    style = MaterialTheme.typography.displaySmall
+                    style = MaterialTheme.typography.displaySmall,
+                    modifier = Modifier.padding(vertical = 32.dp)
                 )
-                Spacer(modifier = Modifier.height(32.dp))
+
                 Step1Section(
                     isSectionFinished = isSection1Finished,
                     onSectionButtonClicked = {
                         val intent = Intent(Settings.ACTION_INPUT_METHOD_SETTINGS)
                         context.startActivity(intent)
+                        sleep(1000)
                         isSection1Finished = true
                     }
                 )
-                Step2Section(
-                    isSectionFinished = isSection2Finished,
-                    onSectionButtonClicked = {
-                        inputMethodManager.showInputMethodPicker()
-                        isSection2Finished = true
-                    }
-                )
-                Step3Section()
-                Spacer(modifier = Modifier.height(32.dp))
+
+                if (isSection1Finished) {
+                    Step2Section(
+                        isSectionFinished = isSection2Finished,
+                        onSectionButtonClicked = {
+                            inputMethodManager.showInputMethodPicker()
+                            sleep(500)
+                            isSection2Finished = true
+                        }
+                    )
+                }
+
+                if (isSection1Finished && isSection2Finished) {
+                    Step3Section()
+                }
             }
         }
     )
