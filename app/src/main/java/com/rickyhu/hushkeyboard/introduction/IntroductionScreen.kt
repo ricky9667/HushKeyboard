4080c298f9dab61c22ac5e0e675fba5179e7ad12
Ricky Hu <ricky9667@gmail.com>
2026-02-24 14:33:05 +0800

    feat: 🎸 adjust IntroductionScreen app bar

--- app/src/main/java/com/rickyhu/hushkeyboard/introduction/IntroductionScreen.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/introduction/IntroductionScreen.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -19,12 +21,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.rickyhu.hushkeyboard.R
 import com.rickyhu.hushkeyboard.theme.HushKeyboardTheme
 import splitties.systemservices.inputMethodManager
 import java.lang.Thread.sleep
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun IntroductionScreen(navigateToHome: () -> Unit = {}) {
     val context = LocalContext.current
@@ -32,6 +34,16 @@ fun IntroductionScreen(navigateToHome: () -> Unit = {}) {
     var isSection2Finished by remember { mutableStateOf(false) }
 
     Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.intro_screen_title),
+                        style = MaterialTheme.typography.headlineMedium,
+                    )
+                },
+            )
+        },
         content = { paddingValues ->
             Column(
                 modifier =
@@ -41,12 +53,6 @@ fun IntroductionScreen(navigateToHome: () -> Unit = {}) {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top,
             ) {
-                Text(
-                    text = stringResource(R.string.intro_screen_title),
-                    style = MaterialTheme.typography.displaySmall,
-                    modifier = Modifier.padding(vertical = 32.dp),
-                )
-
                 Step1Section(
                     isSectionFinished = isSection1Finished,
                     onSectionButtonClicked = {
