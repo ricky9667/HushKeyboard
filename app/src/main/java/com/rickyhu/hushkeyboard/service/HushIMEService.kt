7dfb9024528b0abb9ec3d7909e663f939d4ee89e
Ricky Hu <ricky9667@gmail.com>
2026-02-24 11:13:08 +0800

    refactor: 💡 use koin to manage KeyboardViewModel

--- app/src/main/java/com/rickyhu/hushkeyboard/service/HushIMEService.kt
+++ app/src/main/java/com/rickyhu/hushkeyboard/service/HushIMEService.kt
@@ -1,33 +1,23 @@
 package com.rickyhu.hushkeyboard.service
 
 import android.view.View
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelStore
-import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.setViewTreeLifecycleOwner
 import androidx.savedstate.SavedStateRegistryController
 import androidx.savedstate.SavedStateRegistryOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
-import com.rickyhu.hushkeyboard.data.SettingsRepository
 import com.rickyhu.hushkeyboard.keyboard.HushKeyboardView
 import com.rickyhu.hushkeyboard.keyboard.KeyboardViewModel
-import com.rickyhu.hushkeyboard.keyboard.KeyboardViewModelFactory
-import org.koin.android.ext.android.inject
+import org.koin.android.ext.android.get
 
 class HushIMEService :
     LifecycleInputMethodService(),
-    SavedStateRegistryOwner,
-    ViewModelStoreOwner {
-    private val settingsRepository: SettingsRepository by inject()
+    SavedStateRegistryOwner {
     lateinit var viewModel: KeyboardViewModel
 
     private val savedStateRegistryController = SavedStateRegistryController.create(this)
     override val savedStateRegistry = savedStateRegistryController.savedStateRegistry
 
     override val lifecycle = dispatcher.lifecycle
-
-    private val _viewModelStore = ViewModelStore()
-    override val viewModelStore: ViewModelStore get() = _viewModelStore
 
     override fun onCreateInputView(): View {
         val view = HushKeyboardView(this)
@@ -41,15 +31,6 @@ class HushIMEService :
     override fun onCreate() {
         super.onCreate()
         savedStateRegistryController.performRestore(null)
-        viewModel =
-            ViewModelProvider(
-                owner = this,
-                factory = KeyboardViewModelFactory(settingsRepository),
-            )[KeyboardViewModel::class.java]
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        _viewModelStore.clear()
+        viewModel = get()
     }
 }
